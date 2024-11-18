5982bc9ef56a1cfbf26a0de6bc48fb7ac027dc92
David Nethanel Mashiah <david@mashiahs.com>
2024-11-18 12:05:44 +0200

    Update ubuntu-python3.13-venv.sh

--- ubuntu-python3.13-venv.sh
+++ ubuntu-python3.13-venv.sh
@@ -13,28 +13,31 @@ fi
 VENV_PATH=$1
 
 echo "Updating package list..."
-sudo apt update
+sudo apt update -qq
 
 echo "Installing software-properties-common..."
-sudo apt install -y software-properties-common
+sudo apt install -y -qq software-properties-common
 
 echo "Adding deadsnakes PPA..."
-sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa > /dev/null
 
 echo "Updating package list again..."
-sudo apt update
+sudo apt update -qq
 
 echo "Creating virtual environment at $VENV_PATH..."
-python3.13 -m venv "$VENV_PATH" --without-pip
+python3.13 -m venv "$VENV_PATH/venv" --without-pip
 
 echo "Activating virtual environment..."
-source "$VENV_PATH/bin/activate"
+source "$VENV_PATH/venv/bin/activate"
 
 echo "Downloading get-pip.py..."
-curl -O https://bootstrap.pypa.io/get-pip.py
+curl -s -O https://bootstrap.pypa.io/get-pip.py
 
 echo "Installing pip..."
-python get-pip.py
+python get-pip.py > /dev/null
+
+echo "Cleaning up get-pip.py..."
+rm get-pip.py
 
 echo "Verifying pip installation..."
 pip --version
